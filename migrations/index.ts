ee856649d2d2843e94f689fc25f10cb2b69ac662
LauRRaud <raudsoolaur@gmail.com>
2026-06-21 18:33:57 +0300

    Add orders collection migration

--- migrations/index.ts
+++ migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260621_084547_initial_payload_schema from './20260621_084547_initial_payload_schema';
+import * as migration_20260621_152759_orders_collection from './20260621_152759_orders_collection';
 
 export const migrations = [
   {
     up: migration_20260621_084547_initial_payload_schema.up,
     down: migration_20260621_084547_initial_payload_schema.down,
-    name: '20260621_084547_initial_payload_schema'
+    name: '20260621_084547_initial_payload_schema',
+  },
+  {
+    up: migration_20260621_152759_orders_collection.up,
+    down: migration_20260621_152759_orders_collection.down,
+    name: '20260621_152759_orders_collection'
   },
 ];
